b95a3e8d28612f42471b1c5dfbd86712d3f789e6
Sakib <skb.noshtocod3r@gmail.com>
2018-03-06 11:26:41 +0600

    Moving the bird

---
+++ New Unity Project/Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour 
+{
+	public float speed = 2;
+	public float force = 300;
+	
+	void Start () 
+	{
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+		
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
