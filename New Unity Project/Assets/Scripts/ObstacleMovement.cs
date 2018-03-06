b1795300c0c84f4c366a01c6398b49630a13febe
Sakib <skb.noshtocod3r@gmail.com>
2018-03-06 11:43:15 +0600

    Obstacle Movement Script

---
+++ New Unity Project/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour 
+{
+	public float speed = 0;
+	public float switchTime = 2;
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+		InvokeRepeating("Switch", 0 , switchTime);
+	}
+
+	void Switch()
+	{
+		GetComponent<Rigidbody2D>().velocity *= -1;
+	}
+}
