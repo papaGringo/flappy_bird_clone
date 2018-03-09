4dffeb4dee51cf148c21eb359801b0ec094505f2
papaGringo <skb.noshtocod3r@gmail.com>
2018-03-10 01:53:27 +0600

    New Test

--- New Unity Project/Assets/Scripts/BirdGameManager.cs
+++ New Unity Project/Assets/Scripts/BirdGameManager.cs
@@ -27,11 +27,45 @@ public class BirdGameManager : MonoBehaviour
 	
 	void Start()
 	{
+		obstacleQueue = new Queue<Transform>();
 		InvokeRepeating("Generate", 0, Random.Range(3,5) );
+	}
+
+	void Update()
+	{
+		if(curObsGenerator != null && (birdPosition.position.x > curObsGenerator.position.x))
+		{
+			//score++ ; //curObs = null;
+			FindClosestCurObs();
+		}
+		else
+		{
+			//find closest curObs;
+			FindClosestCurObs();
+
+		}
+	}
+
+	private void FindClosestCurObs()
+	{
+		if(obstacleQueue.Count>0)
+		{
+			curObsGenerator = obstacleQueue.Peek();
+			obstacleQueue.Dequeue();
+		}
 	}
 
 	private void Generate()
 	{
 		Instantiate(obstacleGeneratorPrefab, new Vector3( birdPosition.position.x , Random.Range(-.75f, 1.25f) , birdPosition.position.z ) + offset, birdPosition.rotation);
+		if(curObsGenerator == null)
+		{
+			curObsGenerator = obstacleGeneratorPrefab.transform;
+			obstacleQueue.Enqueue(curObsGenerator);
+		}
 	}
+
+	
+	public Queue<Transform> obstacleQueue;
+	private Transform curObsGenerator;
 }
--- New Unity Project/Assets/Scripts/BirdMovement.cs
+++ New Unity Project/Assets/Scripts/BirdMovement.cs
@@ -6,7 +6,20 @@ public class BirdMovement : MonoBehaviour
 {
 	public float speed = 2;
 	public float force = 300;
+	public float speedX = 3;
 	
+	private float inputX = 0f;
+	private	 float	 inputY = 0f;
+
+	private float inputDelay = 0.2f;
+
+	private int jumpCount = 0;
+	private void GetInput()
+	{
+		inputX = Input.GetAxisRaw("Horizontal");
+		inputY = Input.GetAxisRaw("Jump");
+	}
+
 	void Start () 
 	{
 		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
@@ -14,10 +27,32 @@ public class BirdMovement : MonoBehaviour
 	
 	void Update () 
 	{
+		GetInput();
+		/*
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
 			GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
 		}
+		*/GetComponent<Rigidbody2D>().AddForce(-Vector2.up * force);
+	}
+
+	void FixedUpdate()
+	{
+		// if( Mathf.Abs(inputX) > inputDelay)
+		// {
+		// 	GetComponent<Rigidbody2D>().velocity = Vector2.right * inputX * speedX;
+		// }
+		if(Mathf.Abs(inputY)> inputDelay)
+		{
+			if(jumpCount > 0)
+			{	
+				jumpCount = 0;
+				return;
+			}
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * force, ForceMode2D.Impulse);
+			jumpCount++;
+		}	
+		// GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
